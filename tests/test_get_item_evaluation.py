a62e82a95d084d96f4019dbd9231b619312a3472
sayo9904 <cosmos.mvemjsun@gmail.com>
2025-04-03 01:11:01 +0900

    feat: csvからマップ、季節、アイテムを指定して評価値を返す関数を実装

--- tests/test_get_item_evaluation.py
+++ tests/test_get_item_evaluation.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from get_item_evaluation import get_item_evaluation
 
 
-class TestGetItemValueWithCSV(unittest.TestCase):
+class TestGetItemEvaluation(unittest.TestCase):
     DATA_DIR = Path(os.environ.get("DATADIR") or "")
     TEST_CSV_PATH = DATA_DIR.joinpath("plants/テストの台地.csv")
     TEST_DATA = [
@@ -24,16 +24,16 @@ class TestGetItemValueWithCSV(unittest.TestCase):
         if self.TEST_CSV_PATH.exists():
             self.TEST_CSV_PATH.unlink()
 
-    def test_get_item_evaluation_from_csv(self):
+    def test_get_right_value_from_csv(self):
         """CSVファイルから正しく値を取得できることを確認"""
-        assert get_item_evaluation("テストの台地", "豊穣期", "ドス怪力の種") == 3
-        assert get_item_evaluation("テストの台地", "荒廃期", "忍耐の種亜種") == 0.5
+        assert get_item_evaluation("テストの台地", "豊穣期", "ドス怪力の種") == "3"
+        assert get_item_evaluation("テストの台地", "荒廃期", "忍耐の種亜種") == "0.5"
 
-    def test_get_item_evaluation_not_found_in_csv(self):
+    def test_not_found_keys(self):
         """存在しないアイテムの場合に0を返すことを確認"""
-        assert get_item_evaluation("テストの台地", "狂走エキスG", "異常気象") == 0
+        assert get_item_evaluation("テストの台地", "異常気象", "狂走エキスG") == "0"
 
-    def test_get_item_evaluation_file_not_found(self):
+    def test_file_not_found(self):
         """存在しないファイルを指定したときにFileNotFoundErrorを返すことを確認"""
         with self.assertRaises(FileNotFoundError):
             get_item_evaluation("存在しないマップ", "豊穣期", "ドス怪力の種")
