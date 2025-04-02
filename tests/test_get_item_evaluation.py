94719361911307a7149579425977eec42ed0c9f3
sayo9904 <cosmos.mvemjsun@gmail.com>
2025-04-03 00:04:56 +0900

    mod: エラーハンドリングの仕様を追加

--- tests/test_get_item_evaluation.py
+++ tests/test_get_item_evaluation.py
@@ -6,14 +6,14 @@ from get_item_evaluation import get_item_evaluation
 
 
 class TestGetItemValueWithCSV(unittest.TestCase):
-    DATA_DIR = Path(os.environ.get("DATADIR"))
+    DATA_DIR = Path(os.environ.get("DATADIR") or "")
     TEST_CSV_PATH = DATA_DIR.joinpath("plants/テストの台地.csv")
     TEST_DATA = [
         ["アイテム名", "豊穣期", "荒廃期", "異常気象"],
         ["ドス怪力の種", "3", "2", "1"],
         ["忍耐の種亜種", "0", "0.5", "3"],
     ]
-    
+
     def setUp(self):
         """前処理: テスト用のCSVファイルを作成"""
         with self.TEST_CSV_PATH.open(mode="w", encoding="utf-8") as f:
@@ -32,6 +32,11 @@ class TestGetItemValueWithCSV(unittest.TestCase):
     def test_get_item_evaluation_not_found_in_csv(self):
         """存在しないアイテムの場合に0を返すことを確認"""
         assert get_item_evaluation("テストの台地", "狂走エキスG", "異常気象") == 0
+
+    def test_get_item_evaluation_file_not_found(self):
+        """存在しないファイルを指定したときにFileNotFoundErrorを返すことを確認"""
+        with self.assertRaises(FileNotFoundError):
+            get_item_evaluation("存在しないマップ", "豊穣期", "ドス怪力の種")
 
 
 if __name__ == "__main__":
